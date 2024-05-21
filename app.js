beb70fa0779f26a179d6968cfe1b5dfb0c301a03
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-05-21 10:03:43 -0400

    gameboard now has grid layout

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-const containerEl = document.querySelector('.container');
+const containerEl = document.querySelector('.gameboard');
 
 function init() {
 	for (let i = 0; i < 81; i++) {
 		const sqrEl = document.createElement('div');
 		sqrEl.className = 'sqr';
+		sqrEl.textContent = 'o';
 		containerEl.appendChild(sqrEl);
 	}
 }
