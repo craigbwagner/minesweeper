34268b7210e56d625346b4301427fa3f4c0e1258
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-05-21 11:23:17 -0400

    initiated basic data structure

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
+/*-------------------------------- Constants --------------------------------*/
+
+/*-------------------------------- Variables --------------------------------*/
+let time;
+let won;
+let lost;
+let gameBoard;
+
+/*------------------------ Cached Element References ------------------------*/
 const containerEl = document.querySelector('.gameboard');
 
+/*----------------------------- Event Listeners -----------------------------*/
+
+/*-------------------------------- Functions --------------------------------*/
 function init() {
 	for (let i = 0; i < 81; i++) {
 		const sqrEl = document.createElement('div');
 		sqrEl.className = 'sqr';
 		containerEl.appendChild(sqrEl);
 	}
+	time = 0;
+	won = false;
+	lost = false;
+	gameBoard = new Array(81);
+	for (let i = 0; i < 81; i++) {
+		gameBoard[i] = { revealed: false, value: '' };
+	}
+	console.log(gameBoard);
 }
 
 init();
